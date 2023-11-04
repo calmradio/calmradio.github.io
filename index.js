0d9a9b6e3c99ec3f4caa74d54f35c3b8333e2f63
calmradio <149890104+calmradio@users.noreply.github.com>
2023-11-04 15:55:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,67 +9,53 @@ const firebaseConfig = {
   appId: "1:444493245960:web:79eb5be40b1e6d5cd0ebbd",
   measurementId: "G-3E2KRL3VBC"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize the database
+// initialize database
 const db = firebase.database();
 
-// Get user's data
+// get user's data
 const username = prompt("Enter Username");
 
-// Submit form
-// Listen for the submit event on the form and call the sendMessage function
+// submit form
+// listen for submit event on the form and call the postChat function
 document.getElementById("message-form").addEventListener("submit", sendMessage);
 
-// Send message to the database
+// send message to db
 function sendMessage(e) {
   e.preventDefault();
 
-  // Get values to be submitted
+  // get values to be submitted
   const timestamp = Date.now();
   const messageInput = document.getElementById("message-input");
   const message = messageInput.value;
 
-  // Clear the input box
+  // clear the input box
   messageInput.value = "";
 
-  // Create a database collection and send in the data
+  //auto scroll to bottom
+  document
+    .getElementById("messages")
+    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+  // create db collection and send in the data
   db.ref("messages/" + timestamp).set({
     username,
     message,
   });
-
-  // Display the message immediately
-  displayMessage(username, message);
-
-  // Auto-scroll to bottom
-  scrollToBottom();
 }
 
-// Function to display the message
-function displayMessage(username, message) {
-  const messagesList = document.getElementById("messages");
-  const messageElement = document.createElement('li');
-  messageElement.classList.add(username === username ? 'sent' : 'receive');
-  messageElement.innerHTML = `<span>${username}: </span>${message}`;
-  messagesList.appendChild(messageElement);
-}
-
-// Function to scroll to the bottom of the chat
-function scrollToBottom() {
-  const messagesList = document.getElementById("messages");
-  messagesList.scrollTop = messagesList.scrollHeight - messagesList.clientHeight;
-}
-
-// Display the messages
-// Reference the collection created earlier
+// display the messages
+// reference the collection created earlier
 const fetchChat = db.ref("messages/");
 
-// Check for new messages using the onChildAdded event listener
+// check for new messages using the onChildAdded event listener
 fetchChat.on("child_added", function (snapshot) {
   const messages = snapshot.val();
-  displayMessage(messages.username, messages.message);
-  scrollToBottom();
+  const message = `<li class=${
+    username === messages.username ? "sent" : "receive"
+  }><span>${messages.username}: </span>${messages.message}</li>`;
+  // append the message on the page
+  document.getElementById("messages").innerHTML += message;
 });
