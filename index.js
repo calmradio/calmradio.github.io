d6acd2f7220c54650cfae73afd03cea8057e9509
calmradio <149890104+calmradio@users.noreply.github.com>
2023-11-04 11:02:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ const firebaseConfig = {
   appId: "1:444493245960:web:79eb5be40b1e6d5cd0ebbd",
   measurementId: "G-3E2KRL3VBC"
 };
-
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-	
+
+// initialize database
 const db = firebase.database();
-		
+
+// get user's data
 const username = prompt("Please Tell Us Your Name");
 
+// submit form
+// listen for submit event on the form and call the postChat function
+document.getElementById("message-form").addEventListener("submit", sendMessage);
+
+// send message to db
 function sendMessage(e) {
   e.preventDefault();
 
@@ -31,21 +38,19 @@ function sendMessage(e) {
   document
     .getElementById("messages")
     .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-	
+
   // create db collection and send in the data
-	
   db.ref("messages/" + timestamp).set({
-	
     username,
-	
     message,
-	
   });
-	
 }
 
+// display the messages
+// reference the collection created earlier
 const fetchChat = db.ref("messages/");
-	
+
+// check for new messages using the onChildAdded event listener
 fetchChat.on("child_added", function (snapshot) {
   const messages = snapshot.val();
   const message = `<li class=${
