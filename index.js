2dbb90bc6322393f78627838f5724af62ee1d2af
calmradio <149890104+calmradio@users.noreply.github.com>
2023-11-06 17:37:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCyTeJ8Jrynxer1qYJ16BVwGcXkhq5ZEVs",
@@ -9,77 +10,53 @@ const firebaseConfig = {
   appId: "1:444493245960:web:79eb5be40b1e6d5cd0ebbd",
   measurementId: "G-3E2KRL3VBC"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize database
+// initialize database
 const db = firebase.database();
 
-// Get user's data
+// get user's data
 const username = prompt("Please Tell Us Your Name");
 
-// Submit form
-// Listen for submit event on the form and call the sendMessage function
+// submit form
+// listen for submit event on the form and call the postChat function
 document.getElementById("message-form").addEventListener("submit", sendMessage);
 
-// Send message to db
+// send message to db
 function sendMessage(e) {
   e.preventDefault();
 
-  // Get values to be submitted
+  // get values to be submitted
   const timestamp = Date.now();
   const messageInput = document.getElementById("message-input");
   const message = messageInput.value;
 
-  // Clear the input box
+  // clear the input box
   messageInput.value = "";
 
-  // Auto-scroll to bottom
+  //auto scroll to bottom
   document
     .getElementById("messages")
     .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 
-  // Create db collection and send in the data
+  // create db collection and send in the data
   db.ref("messages/" + timestamp).set({
     username,
     message,
   });
 }
 
-// Display the messages
-// Reference the collection created earlier
+// display the messages
+// reference the collection created earlier
 const fetchChat = db.ref("messages/");
 
-// Check for new messages using the onChildAdded event listener
+// check for new messages using the onChildAdded event listener
 fetchChat.on("child_added", function (snapshot) {
   const messages = snapshot.val();
   const message = `<li class=${
     username === messages.username ? "sent" : "receive"
   }><span>${messages.username}: </span>${messages.message}</li>`;
-  // Append the message on the page
+  // append the message on the page
   document.getElementById("messages").innerHTML += message;
-});
-
-// Get a reference to the 'userCount' node in the database
-const userCountRef = db.ref('userCount');
-
-// Increment the user count when a user visits the website
-userCountRef.transaction((currentCount) => {
-  // If the currentCount is null, it means no data exists yet, so set it to 1
-  if (currentCount === null) {
-    return 1;
-  } else {
-    // Increment the current count by 1
-    return currentCount + 1;
-  }
-});
-
-// Display the live user count
-const userCountElement = document.getElementById('userCount');
-
-// Listen for changes in the user count
-userCountRef.on('value', (snapshot) => {
-  const userCount = snapshot.val();
-  userCountElement.innerText = userCount;
 });
