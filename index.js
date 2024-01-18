e3e6d608368386d468394ba4c3b91b5ee560d60b
calmradio <149890104+calmradio@users.noreply.github.com>
2024-01-18 14:00:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.database();
+// const db = firebase.database();
 
 const MAX_MESSAGES_DISPLAYED = 25;
 const SIX_HOURS_IN_MS = 6 * 60 * 60 * 1000;
 
 //const username = prompt("Please Tell Us Your Name");
 
-document.getElementById("message-form").addEventListener("submit", sendMessage);
+// document.getElementById("message-form").addEventListener("submit", sendMessage);
 
-function sendMessage(e) {
+// function sendMessage(e) {
   e.preventDefault();
 
   const timestamp = Date.now();
